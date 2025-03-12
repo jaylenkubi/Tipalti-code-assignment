67b0250a4d5ca1241b820c868f94f2910c22c51f
jaylenkubi <jaylenkubi@gmail.com>
2025-03-12 04:55:58 +0000

    removed comment from main.tsx

--- react-ts-starter-master/src/main.tsx
+++ react-ts-starter-master/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// No need to import Mantine styles in v6 as they are included in the components
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
